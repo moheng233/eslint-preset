4d9f4c6c09e4f91936ac13e4ed546058fb433bcb
墨恒 <ylm200322400@163.com>
2024-09-30 16:35:42 +0800

    ✨ feat: Customising the root directory

--- src/index.ts
+++ src/index.ts
@@ -8,37 +8,39 @@ import tseslint from "typescript-eslint";
 
 export { config } from "typescript-eslint";
 
-export const preset: TSESLint.FlatConfig.ConfigArray = tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    perfectionist.configs["recommended-natural"],
-    stylistic.configs.customize({
-        flat: true,
-        indent: 4,
-        quotes: "double",
-        semi: true,
-        braceStyle: "stroustrup",
-        commaDangle: "only-multiline"
-    }),
-    {
-        plugins: {
-            "import-x": importx
-        },
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname
+export function preset(rootPath: string) {
+    return tseslint.config(
+        eslint.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked,
+        perfectionist.configs["recommended-natural"],
+        stylistic.configs.customize({
+            flat: true,
+            indent: 4,
+            quotes: "double",
+            semi: true,
+            braceStyle: "stroustrup",
+            commaDangle: "only-multiline"
+        }),
+        {
+            plugins: {
+                "import-x": importx
+            },
+            languageOptions: {
+                parserOptions: {
+                    projectService: true,
+                    tsconfigRootDir: rootPath
+                }
+            },
+            rules: {
+                "perfectionist/sort-object-types": "off",
+                "perfectionist/sort-interfaces": "off",
+                "perfectionist/sort-classes": "off",
+                "perfectionist/sort-objects": "off",
+                "perfectionist/sort-enums": "off",
+                "perfectionist/sort-sets": "off",
+                "perfectionist/sort-maps": "off",
+                "import-x/consistent-type-specifier-style": ["error", "prefer-top-level"]
             }
-        },
-        rules: {
-            "perfectionist/sort-object-types": "off",
-            "perfectionist/sort-interfaces": "off",
-            "perfectionist/sort-classes": "off",
-            "perfectionist/sort-objects": "off",
-            "perfectionist/sort-enums": "off",
-            "perfectionist/sort-sets": "off",
-            "perfectionist/sort-maps": "off",
-            "import-x/consistent-type-specifier-style": ["error", "prefer-top-level"]
         }
-    }
-);
+    );
+};
